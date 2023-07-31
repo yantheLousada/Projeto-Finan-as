759a94746bd360bcd9c66267b7d1f0ba9fcebc39
yantheLousada <yanthe96@hotmail.com>
2023-07-31 11:22:23 -0300

    Header commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <header>
+        <a href="https://www.infomoney.com.br/">Notícias</a>
+      </header>
+    </>
+  );
+}
+
+export default App;
